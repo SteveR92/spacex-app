20ec3b3ac6d035d4bfc17dc6cbc4b535c2819521
Steve Roe <steve@phramesco.com>
2020-06-01 20:24:45 +0900

    added youtube link

--- components/flight_info/MissionName.js
+++ components/flight_info/MissionName.js
@@ -5,6 +5,7 @@ const MissionName = ({ props }) => {
   let unixDate = props.launch_date_unix;
   const dateObj = new Date(unixDate * 1000);
   const date = JSON.stringify(dateObj).slice(1, 11);
+  let youtubeLink = props.links.youtube_id;
   return (
     <div className={styles.infoCard}>
       <div className={styles.infoHeader}>
@@ -19,6 +20,15 @@ const MissionName = ({ props }) => {
       <p>
         <span id={styles.key}>Launch Date: </span>
         {date}
+      </p>
+      <p>
+        <span id={styles.key}>YouTube: </span>
+        <a
+          href={`https://www.youtube.com/watch?v=${youtubeLink}`}
+          target="_blank"
+        >
+          External Link
+        </a>
       </p>
     </div>
   );
